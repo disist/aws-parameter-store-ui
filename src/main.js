6c15cfd998ec76919b2c9bf118b26c0b7ce543e1
Dmytro Golysh <disist357@gmail.com>
2019-06-25 02:20:15 +0300

    feat(aws): init getting ssm feature

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Table from 'buefy/dist/components/table';
+import Input from 'buefy/dist/components/input';
+import Button from 'buefy/dist/components/button';
+import Field from 'buefy/dist/components/field';
+import 'buefy/dist/buefy.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(Table);
+Vue.use(Input);
+Vue.use(Button);
+Vue.use(Field);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
