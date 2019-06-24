b566ac289e2944aabd4ae9afd5af26155aa62875
Dmytro Golysh <disist357@gmail.com>
2019-06-25 02:37:31 +0300

    feat(pages): test deploy to pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
     productionSourceMap: false,
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/aws-parameter-store-ui/'
-        : '/'
+    publicPath: '/'
 };
