690d0253ad91553d693685c6e1a8d6d093d5f00b
Dmytro Golysh <disist357@gmail.com>
2019-06-25 13:26:12 +0300

    fix(pages): Fix demo deployment to pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
     productionSourceMap: false,
-    publicPath: '/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/aws-parameter-store-ui/'
+        : '/'
 };
