675c4aa97f78a3b15e7d6fd8374fb434da1bc1ca
Dmytro Golysh <disist357@gmail.com>
2019-06-25 19:12:11 +0300

    fix(aws): Add ability to locally search with or pattern

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Input from 'buefy/dist/components/input';
 import Button from 'buefy/dist/components/button';
 import Field from 'buefy/dist/components/field';
 import Select from 'buefy/dist/components/select';
+import TagInput from 'buefy/dist/components/taginput';
 import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
@@ -14,6 +15,7 @@ Vue.use(Input);
 Vue.use(Button);
 Vue.use(Field);
 Vue.use(Select);
+Vue.use(TagInput);
 
 new Vue({
   render: h => h(App),
