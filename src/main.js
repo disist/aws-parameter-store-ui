f1db7184043fc379ef4713e5ecf9d00fabd60797
Dmytro Golysh <disist357@gmail.com>
2019-06-26 11:37:22 +0300

    fix(aws): Bring back typehead search

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import Input from 'buefy/dist/components/input';
 import Button from 'buefy/dist/components/button';
 import Field from 'buefy/dist/components/field';
 import Select from 'buefy/dist/components/select';
-import TagInput from 'buefy/dist/components/taginput';
 import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
@@ -15,7 +14,6 @@ Vue.use(Input);
 Vue.use(Button);
 Vue.use(Field);
 Vue.use(Select);
-Vue.use(TagInput);
 
 new Vue({
   render: h => h(App),
