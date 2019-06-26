5d708214ad5dc4fc3ebd25d30f38446fa11253b2
Dmytro Golysh <disist357@gmail.com>
2019-06-26 19:32:56 +0300

    fix(aws): Add ability to change the value

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import Input from 'buefy/dist/components/input';
 import Button from 'buefy/dist/components/button';
 import Field from 'buefy/dist/components/field';
 import Select from 'buefy/dist/components/select';
+import Loading from 'buefy/dist/components/loading';
+import Modal from 'buefy/dist/components/modal';
+import Toast from 'buefy/dist/components/toast';
 import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
@@ -14,6 +17,9 @@ Vue.use(Input);
 Vue.use(Button);
 Vue.use(Field);
 Vue.use(Select);
+Vue.use(Loading);
+Vue.use(Modal);
+Vue.use(Toast);
 
 new Vue({
   render: h => h(App),
