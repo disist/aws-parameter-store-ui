164a502bcf353575a771d0b3e985ec9782433999
Dmytro Golysh <disist357@gmail.com>
2019-07-03 15:46:09 +0300

    fix(aws): Add support to use several profiles

--- src/components/ParameterStoreList.vue
+++ src/components/ParameterStoreList.vue
@@ -1,54 +1,95 @@
 <template>
     <div>
         <div class="controls">
-            <div class="controls__card card">
-                <section>
-                    <b-field label="accessKeyId">
-                        <b-input v-model="accessKeyId" placeholder="accessKeyId"></b-input>
-                    </b-field>
-                    <b-field label="secretAccessKey">
-                        <b-input type="password" v-model="secretAccessKey" placeholder="secretAccessKey"
-                                 password-reveal></b-input>
-                    </b-field>
-                    <b-field label="region">
-                        <b-input v-model="region" placeholder="region"></b-input>
-                    </b-field>
-                </section>
-                <div class="buttons controls__login-buttons">
-                    <b-button :disabled="ssm" @click="sign()">Sign</b-button>
+
+            <b-collapse
+                    aria-id="contentIdForLoginCard"
+                    class="panel"
+                    :open.sync="isOpenLoginCard">
+                <div
+                        slot="trigger"
+                        class="panel-heading controls__card-title"
+                        role="button"
+                        aria-controls="contentIdForLoginCard">
+                    <strong>Login Profile: {{activeProfileName}}</strong>
                 </div>
-            </div>
+                <div class="controls__card card">
+                    <section>
+                        <b-dropdown aria-role="list">
+                            <button class="button is-primary" slot="trigger">
+                                <span>Profiles</span>
+                            </button>
 
-            <div class="controls__card card">
+                            <b-dropdown-item
+                                    aria-role="listitem"
+                                    v-for="(profile, index) in profiles"
+                                    v-bind:key="profile.name"
+                                    @click="onProfileClick(index)">
+                                {{profile.name}}</b-dropdown-item>
+                            <hr>
+                            <b-dropdown-item aria-role="listitem" @click="createNewProfile()">Create New</b-dropdown-item>
+                        </b-dropdown>
 
-                <b-field>
-                    <b-button type="is-primary" :disabled="!ssm" :loading="loading" @click="getAllParameters()">Get all
-                        parameters
-                    </b-button>
+                        <b-field label="accessKeyId">
+                            <b-input v-model="accessKeyId" placeholder="accessKeyId"></b-input>
+                        </b-field>
+                        <b-field label="secretAccessKey">
+                            <b-input type="password" v-model="secretAccessKey" placeholder="secretAccessKey"
+                                     password-reveal></b-input>
+                        </b-field>
+                        <b-field label="region">
+                            <b-input v-model="region" placeholder="region"></b-input>
+                        </b-field>
+                    </section>
+                    <div class="buttons controls__login-buttons">
+                        <b-button :disabled="ssm" @click="sign()">Sign</b-button>
+                    </div>
+                </div>
+            </b-collapse>
 
-                    <span class="controls__path-label">By path: </span>
+            <b-collapse
+                    aria-id="contentIdForFiltersCard"
+                    class="panel"
+                    :open.sync="isOpenFiltersCard">
+                <div
+                        slot="trigger"
+                        class="panel-heading controls__card-title"
+                        role="button"
+                        aria-controls="contentIdForFiltersCard">
+                    <strong>Filters</strong>
+                </div>
+                <div class="controls__card card">
 
-                    <b-input v-model="path" :disabled="this.loading" placeholder="by path"></b-input>
-                </b-field>
+                    <b-field>
+                        <b-button type="is-primary" :disabled="!ssm" :loading="loading" @click="getAllParameters()">Get all
+                            parameters
+                        </b-button>
 
-                <hr>
+                        <span class="controls__path-label">By path: </span>
 
-                <b-field label="Local Filter, Search by:">
-                    <b-select v-model="searchField" :disabled="!ssm" placeholder="Select a Field">
-                        <option
-                                v-for="option in ['Name', 'Value']"
-                                :value="option"
-                                :key="option">
-                            {{ option }}
-                        </option>
-                    </b-select>
-                </b-field>
+                        <b-input v-model="path" :disabled="this.loading" placeholder="by path"></b-input>
+                    </b-field>
 
-                <b-field>
-                    <b-input v-model="searchText" :disabled="!ssm" placeholder="Search value" rounded></b-input>
-                </b-field>
+                    <hr>
 
-            </div>
+                    <b-field label="Local Filter, Search by:">
+                        <b-select v-model="searchField" :disabled="!ssm" placeholder="Select a Field">
+                            <option
+                                    v-for="option in ['Name', 'Value']"
+                                    :value="option"
+                                    :key="option">
+                                {{ option }}
+                            </option>
+                        </b-select>
+                    </b-field>
+
+                    <b-field>
+                        <b-input v-model="searchText" :disabled="!ssm" placeholder="Search value" rounded></b-input>
+                    </b-field>
+
+                </div>
+            </b-collapse>
+
         </div>
 
         <div class="container parameter-store-list">
@@ -82,11 +123,19 @@
 <script>
     import SSM from 'aws-sdk/clients/ssm';
     import ParameterModal from './ParameterModal';
+    import ProfileModal from './ProfileModal';
 
     export default {
         name: 'ParameterStoreList',
         data: function () {
             return {
+                isOpenLoginCard: true,
+                isOpenFiltersCard: true,
+
+                profiles: null,
+                activeProfile: null,
+                activeProfileName: '',
+
                 accessKeyId: '',
                 secretAccessKey: '',
                 region: '',
@@ -112,11 +161,29 @@
             }
         },
         created: function () {
-            this.accessKeyId = localStorage.getItem('accessKeyId');
-            this.secretAccessKey = localStorage.getItem('secretAccessKey');
-            this.region = localStorage.getItem('region');
+            this.init();
         },
         methods: {
+            init() {
+                this.profiles = JSON.parse(localStorage.getItem('profiles'));
+                const activeProfile = localStorage.getItem('activeProfile');
+
+                if (activeProfile) {
+                    this.activeProfile = +activeProfile;
+                }
+
+                if (!this.profiles) {
+                    this.profiles = [{
+                        name: 'Common'
+                    }];
+
+                    this.activeProfile = 0;
+                }
+
+                if (this.profiles) {
+                    this._readProfile(this.activeProfile);
+                }
+            },
             sign() {
                 this.ssm = new SSM({
                     accessKeyId: this.accessKeyId,
@@ -124,9 +191,34 @@
                     region: this.region
                 });
 
-                localStorage.setItem('accessKeyId', this.accessKeyId);
-                localStorage.setItem('secretAccessKey', this.secretAccessKey);
-                localStorage.setItem('region', this.region);
+                this.profiles[this.activeProfile].accessKeyId = this.accessKeyId;
+                this.profiles[this.activeProfile].secretAccessKey = this.secretAccessKey;
+                this.profiles[this.activeProfile].region = this.region;
+
+                this._saveProfile();
+
+                this.isOpenLoginCard = false;
+            },
+            onProfileClick(index) {
+                this.activeProfile = index;
+                this._readProfile(this.activeProfile);
+
+                this._saveProfile();
+
+                this.ssm = null;
+            },
+            createNewProfile() {
+                this.$modal.open({
+                    parent: this,
+                    component: ProfileModal,
+                    hasModalCard: true,
+                    props: {
+                        item: null
+                    },
+                    events: {
+                        'profile:created': this.onProfileCreated
+                    }
+                });
             },
             getAllParameters() {
                 this.loading = true;
@@ -173,7 +265,7 @@
                 const loadingComponent = this.$loading.open({isFullPage: true});
 
                 Promise.all([
-                    this.ssm.getParameter({Name: item.Name }).promise(),
+                    this.ssm.getParameter({Name: item.Name}).promise(),
                     this.ssm.getParameterHistory({Name: item.Name}).promise()
                 ]).then(([parameter, parameterHistory]) => {
                     loadingComponent.close();
@@ -189,6 +281,21 @@
                         }
                     });
                 });
+            },
+            onProfileCreated() {
+                this.ssm = null;
+
+                this.init();
+            },
+            _readProfile(index) {
+                this.accessKeyId = this.profiles[index].accessKeyId;
+                this.secretAccessKey = this.profiles[index].secretAccessKey;
+                this.region = this.profiles[index].region;
+                this.activeProfileName = this.profiles[index].name;
+            },
+            _saveProfile() {
+                localStorage.setItem('profiles', JSON.stringify(this.profiles));
+                localStorage.setItem('activeProfile', this.activeProfile);
             }
         }
     }
@@ -201,6 +308,10 @@
         justify-content: space-evenly;
 
         margin-bottom: 20px;
+    }
+
+    .controls__card-title {
+        width: 500px;
     }
 
     .controls__card {
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import Select from 'buefy/dist/components/select';
 import Loading from 'buefy/dist/components/loading';
 import Modal from 'buefy/dist/components/modal';
 import Toast from 'buefy/dist/components/toast';
+import Collapse from 'buefy/dist/components/collapse';
+import Dropdown from 'buefy/dist/components/dropdown';
 import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
@@ -20,7 +22,9 @@ Vue.use(Select);
 Vue.use(Loading);
 Vue.use(Modal);
 Vue.use(Toast);
+Vue.use(Collapse);
+Vue.use(Dropdown);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
